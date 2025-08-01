b7081cd5aa870e24b0f6cbda8b6e4e3b07a1a3f8
Aryaman Singh <your-email@example.com>
2025-08-01 11:22:09 +0530

    updates

--- app/api/Email_send_test.py
+++ app/api/Email_send_test.py
@@ -4,4 +4,4 @@ client_secret_file = 'gmail_api.json'
 API_SERVICE_NAME = 'gmail'
 API_VERSION = 'v1'
 SCOPES = ['https://mail.google.com/']
-service = create_service(client_secret_file,API_SERVICE_NAME,API_VERSION,SCOPES)
+service = create_service(client_secret_file, API_SERVICE_NAME, API_VERSION, SCOPES)
